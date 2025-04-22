f7367cea2dc80cf94cac6542aa4f3f435d8c8193
David Moll <dlmoll@proton.me>
2025-04-22 15:33:04 +0200

    Update about-page

--- src/_includes/js/email.js
+++ src/_includes/js/email.js
@@ -1,5 +1,5 @@
 // update this variable with your email :)
-const email = "foobar@gmail.com";
+const email = "contact@davidmoll.net";
 const submit = document.querySelector(".reach-me");
 
 function sendMail() {
